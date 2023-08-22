7e6ebe4e28825cb84e1423271fcbcf2671da6ac2
Tangerie <josh.cavill01@gmail.com>
2023-08-22 11:50:44 +1000

    finally working

--- Mods/SloMoMod/src/main.cpp
+++ Mods/SloMoMod/src/main.cpp
@@ -6,6 +6,7 @@
 #include <Unreal/ReflectedFunction.hpp>
 #include <Unreal/GameplayStatics.hpp>
 #include <Unreal/Hooks.hpp>
+#include <Unreal/World.hpp>
 #include <Input/Handler.hpp>
 
 #include <ModUtils/Extensions.hpp>
@@ -16,34 +17,37 @@ using namespace RC;
 using namespace RC::Unreal;
 
 namespace HLCPPTutorial {
-
-class SloMoMod : public CppUserModBase
-{
-public:
-    SloMoMod() : CppUserModBase()
-	{
-		ModName = STR("SloMoMod");
-		ModVersion = STR("1.0");
-		ModDescription = STR("103 - SloMoMod");
-		ModAuthors = STR("HLCPPTutorial");
-	}
-
-	~SloMoMod()
-	{
-	}
-
+class UGameplayStaticsExtended : public UObject {
 	// Helper from Unreal submodule helps us automatically reference UFunctions
 	struct StaticFunctions {
-		static inline StaticReflectedFunctionBase<UGameplayStatics> GetGlobalTimeDilation{STR("/Script/Engine.GameplayStatics:GetGlobalTimeDilation")};
-		static inline StaticReflectedFunctionBase<UGameplayStatics> SetGlobalTimeDilation{STR("/Script/Engine.GameplayStatics:SetGlobalTimeDilation")};
+		static inline StaticReflectedFunctionBase<UGameplayStaticsExtended> GetGlobalTimeDilation{STR("/Script/Engine.GameplayStatics:GetGlobalTimeDilation")};
+		static inline StaticReflectedFunctionBase<UGameplayStaticsExtended> SetGlobalTimeDilation{STR("/Script/Engine.GameplayStatics:SetGlobalTimeDilation")};
 	};
 
-	static float GetGlobalTimeDilation() {
-		if (!UObjectGlobals::StaticFindObject<UGameplayStatics*>(nullptr, nullptr, STR("/Script/Engine.Default__GameplayStatics"))) {
+public:
+	static inline UGameplayStaticsExtended* Self{nullptr};
+
+private:
+	static bool VerifySelf() {
+		if (!Self)
+		{
+			Self = UObjectGlobals::StaticFindObject<UGameplayStaticsExtended*>(nullptr, nullptr, STR("/Script/Engine.Default__GameplayStatics"));
+			if (!Self)
+			{
+				Output::send(STR("Was unable to locate GameplayStatics\n"));
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+public:
+	static float GetGlobalTimeDilation(UObject* WorldContextObject) {
+		if (!VerifySelf()) {
 			return -1;
 		}
 		if (!StaticFunctions::GetGlobalTimeDilation.IsValid()) {
-			LOG("GetGlobalTimeDilation Is Invalid");
 			return 0;
 		}
 
@@ -53,16 +57,19 @@ public:
 			float ReturnValue;
 		};
 
-		Params p {};
+		Params p {
+			.WorldContextObject = WorldContextObject
+		};
 
 		StaticFunctions::GetGlobalTimeDilation(p);
-
 		return p.ReturnValue;
 	}
 
-	static void SetGlobalTimeDilation(float TimeDilation) {
+	static void SetGlobalTimeDilation(UObject* WorldContextObject, float TimeDilation) {
+		if (!VerifySelf()) {
+			return;
+		}
 		if (!StaticFunctions::SetGlobalTimeDilation.IsValid()) {
-			LOG("SetGlobalTimeDilation Is Invalid");
 			return;
 		}
 
@@ -73,32 +80,55 @@ public:
 			void* ReturnValue;
 		};
 
-		Params p {
+		Params p{
+			.WorldContextObject = WorldContextObject,
 			.TimeDilation = TimeDilation
 		};
 
-		//StaticFunctions::SetGlobalTimeDilation(p);
+		StaticFunctions::SetGlobalTimeDilation(p);
 
 		return;
+	}
+};
+
+class SloMoMod : public CppUserModBase
+{
+public:
+    SloMoMod() : CppUserModBase()
+	{
+		ModName = MOD_NAME;
+		ModVersion = STR("1.0");
+		ModDescription = STR("103 - SloMoMod");
+		ModAuthors = STR("HLCPPTutorial");
+	}
+
+	~SloMoMod()
+	{
 	}
 
 	float currentDilation = 0;
 
 	void on_unreal_init() override {
 		LOG("Initialized");
+		Hook::RegisterBeginPlayPostCallback([&](AActor* Context) {
+			LOG("Begin Play");
+			LOG_EXPR(Context->GetFullName());
+			LOG_EXPR(UGameplayStaticsExtended::GetGlobalTimeDilation(Context));
+			UGameplayStaticsExtended::SetGlobalTimeDilation(Context, 200);
+		});
 	}
 
 	auto on_program_start() -> void override {
-		LOG("Started");
+
 	}
 
-	int tick = -500;
+	//int tick = -6000;
 	auto on_update() -> void override
 	{
-		tick++;
+		/*tick++;
 		if (tick % 600 == 0) {
-			LOG_EXPR(GetGlobalTimeDilation());
-		}
+			LOG_EXPR(UGameplayStaticsExtended::GetGlobalTimeDilation());
+		}*/
 	}
 };
 
